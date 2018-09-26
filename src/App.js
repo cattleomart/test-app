83b18e8647e92189039e3205147c9bbbe3963ce0
Cathal O'Brien <Cathal@soops.intern>
2018-09-26 17:38:52 +0200

    tut 5 1:45

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Login from "./Login.js";
+import SuccessMessage from "./SuccessMessage.js";
+import "./App.css";
 
 class App extends Component {
+  state = { 
+    complete: false, 
+    firstName: ''
+  
+  };
+
+  handleSubmit = e => {
+    e.preventDefault()
+    this.setState({ complete: true })
+    document.cookie = `firstName=${this.state.firstName}`
+     
+  };
+
+  handleInput = e => {
+    this.setState({ firstName: e.currentTarget.value})
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title" data-testid="h1">
+            Welcome to React
+          </h1>
+
+          <nav className="navbar" role="navigation" data-testid="navbar">
+            <ul>
+              <li className="nav-li" data-testid="nav-li">
+                <a href="#">Home</a>
+              </li>
+              <li className="nav-li" data-testid="nav-li">
+                <a href="#">About</a>
+              </li>
+              <li className="nav-li" data-testid="nav-li">
+                <a href="#">Skills</a>
+              </li>
+              <li className="nav-li" data-testid="nav-li">
+                <a href="#">Works</a>
+              </li>
+            </ul>
+          </nav>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.state.complete ? (
+          <SuccessMessage />
+        ) : (
+          <Login submit={this.handleSubmit} input={this.handleInput} />
+        )}
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,35 +1,103 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+const faker = require("faker");
+const devices = require('puppeteer/DeviceDescriptors');
+const iPhone = devices['iPhone 6']
+
+const user = {
+  email: faker.internet.email(),
+  password: "test",
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName()
+};
+
 const isDebugging = () => {
   const debugging_mode = {
     headless: false,
     slowMo: 250,
     devtools: true
-    //executablePath: string,
-    //timeout: number,
-    //ignoreHTTPSErrors: bool
+  
   };
-  return process.env.NODE_ENV === "test" ? debugging_mode : {};
+  return process.env.NODE_ENV === "debug" ? debugging_mode : {};
 };
 
-describe('on page load', () => {
-  test(
-    'h1 loads correctly',
-    async () => {
-      let browser = await puppeteer.launch(isDebugging());
-      let page = await browser.newPage();
-      page.emulate({
-        viewport: {
-          width: 500,
-          height: 2400
-        },
-        userAgent: ""
-      });
-      await page.goto('http://localhost:3000');
-      const html = await page.$eval('.App-title', e => e.innerHTML);
+let browser;
+let page;
+beforeAll(async () => {
+  browser = await puppeteer.launch(isDebugging());
+  page = await browser.newPage();
+  
+ 
 
-      expect(html).toBe('Welcome to React');
-      browser.close();
+  await page.goto("http://localhost:3000");
+  await page.emulate(iPhone);
+});
+
+describe("on page load", () => {
+  test(
+    "h1 loads correctly",
+    async () => {
+      const html = await page.$eval('[data-testid="h1"]', e => e.innerHTML);
+
+      expect(html).toBe("Welcome to React");
     },
     16000
   );
+
+  test("nav loads correctly", async () => {
+    const navbar = await page.$eval('[data-testid="navbar"]',
+      el => (el ? true : false)
+    );
+    const listItems = await page.$$('[data-testid="nav-li"]');
+
+    expect(navbar).toBe(true);
+    expect(listItems.length).toBe(4);
+  });
+
+  describe('login form',()=>{
+    test(
+      "fills out form and submits",
+      async () => {
+  
+        
+  
+        const firstName = await page.$('[data-testid="firstName"]')
+        const lastName = await page.$('[data-testid="lastName"]')
+        const email = await page.$('[data-testid="email"]')
+        const password = await page.$('[data-testid="password"]')
+        const submit = await page.$('[data-testid="submit"]')
+  
+        await firstName.tap()
+        await page.type('[data-testid="firstName"]', user.firstName);
+  
+        await lastName.tap()
+        await page.type('[data-testid="lastName"]', user.lastName);
+  
+        await email.tap()
+        await page.type('[data-testid="email"]', user.email);
+  
+        await password.tap()
+        await page.type('[data-testid="password"]', user.password);
+  
+        await submit.tap()
+  
+        await page.waitForSelector('[data-testid="success"]');
+      },
+      16000
+    );
+    test('sets firstName cookie', async () => {
+      const cookies = await page.cookies();
+      const firstNameCookie = cookies.find(c => c.name === 'firstName' && c.value === user.firstName)
+console.log(firstNameCookie)
+      expect(firstNameCookie).not.toBeUndefined()
+    })
+
+  })
+
+  
+});
+
+afterAll(() => {
+  if (isDebugging()) {
+    browser.close();
+  }
 });
